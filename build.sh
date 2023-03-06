8a1df651c71131815b5056d022317ba915e43cda
Projckt <projckt@protonmail.com>
2023-03-06 18:12:11 +0530

    fixes

--- build.sh
+++ build.sh
@@ -1,16 +1,35 @@
+# Cleanup previous prod
 cd ..
 rm -rf prod
 mkdir prod
+
+# Init Git prod
 cd prod
 git init
 git remote add origin git@github.com-slim-dl:slim-dl/app-prod.git
+
+# Pre-build app-server
 cd ..
 cd dev
 cd app-server 
-npm run pfd 
+
+# Build app-server
+rm -rf dest
+npm run build
+rsync -av dest/ ../../prod
+cp {.gitignore,package.json} ../prod
+
+
+# Pre-build app-frontend
 cd .. 
 cd app-frontend 
-npm run pfd
+
+# Build app-frontend
+rm -rf www
+npm run build --prerender
+rsync -av --delete www/ ../../prod/www
+
+# Push prod 
 cd ..
 cd ..
 cd prod
