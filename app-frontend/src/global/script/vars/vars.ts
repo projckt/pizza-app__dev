706228f99883eb29240a9929272b603bd61f663d
Projckt <projckt@protonmail.com>
2023-03-07 09:20:11 +0530

    fixes

---
+++ app-frontend/src/global/script/vars/vars.ts
@@ -0,0 +1,14 @@
+export const vars = {
+  dev: {
+    api: {
+      url: 'http://localhost',
+      port: 5555,
+    },
+  },
+  prod: {
+    api: {
+      url: 'https://api.slimdl.com',
+      port: -1,
+    },
+  },
+};
