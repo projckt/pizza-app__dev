469be3b045dd8775cb6b2c39d7cfeaeaea06b72e
Projckt <projckt@protonmail.com>
2023-03-07 08:34:27 +0530

    fixes

--- app-frontend/stencil.config.ts
+++ app-frontend/stencil.config.ts
@@ -1,15 +1,12 @@
 import { Config } from '@stencil/core';
 
-// https://stenciljs.com/docs/config
-
 export const config: Config = {
-  globalStyle: 'src/global/app.css',
-  globalScript: 'src/global/app.ts',
+  globalStyle: 'src/global/style/index.css',
+  globalScript: 'src/global/script/index.ts',
   taskQueue: 'async',
   outputTargets: [
     {
       type: 'www',
-      // comment the following line to disable service workers in production
       serviceWorker: null,
       baseUrl: 'https://myapp.local/',
     },
