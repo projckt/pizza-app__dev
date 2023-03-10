3d6bf8b462d7320c210838bef80bd3ca32f377b5
Projckt <projckt@protonmail.com>
2023-03-10 14:13:09 +0530

    fixes logout error messages

--- app-frontend/src/components/patterns/p-user-control/helpers/api/helper_UserControl_Api_Logout.ts
+++ app-frontend/src/components/patterns/p-user-control/helpers/api/helper_UserControl_Api_Logout.ts
@@ -24,8 +24,8 @@ export const helper_UserControl_Api_Logout = async () => {
     });
 
   if (!isSuccess_Logout_Submission) {
-    return { isSuccess_Logout_Submission: false, message_Logout_Submission: 'Signup failed', payload_Logout_Submission: {} };
+    return { isSuccess_Logout_Submission: false, message_Logout_Submission: 'Logout failed', payload_Logout_Submission: {} };
   } else {
-    return { isSuccess_Logout_Submission: true, message_Logout_Submission: 'Signup successful', payload_Logout_Submission: payload_Logout_Submission };
+    return { isSuccess_Logout_Submission: true, message_Logout_Submission: 'Logout successful', payload_Logout_Submission: payload_Logout_Submission };
   }
 };
