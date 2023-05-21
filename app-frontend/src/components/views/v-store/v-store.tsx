1a5523debae5172d2159476b78ea39e5a4793851
Projckt <projckt@protonmail.com>
2023-05-21 10:30:43 +0530

    changes sub_title to edition

--- app-frontend/src/components/views/v-store/v-store.tsx
+++ app-frontend/src/components/views/v-store/v-store.tsx
@@ -197,7 +197,7 @@ export class VStore {
             <p-publication
               id_Publication={publication.id}
               heading={publication.title}
-              sub_Heading={publication.sub_title}
+              sub_Heading={publication.edition}
               description={publication.description}
               url_Sample={publication.url_sample}
               url_Toc={publication.url_toc}
