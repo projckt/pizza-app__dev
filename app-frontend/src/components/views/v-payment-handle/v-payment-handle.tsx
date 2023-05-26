42007cd29dad1c3f1f1c48b208cac62d5319a027
Projckt <projckt@protonmail.com>
2023-05-26 13:31:32 +0530

    fixes

--- app-frontend/src/components/views/v-payment-handle/v-payment-handle.tsx
+++ app-frontend/src/components/views/v-payment-handle/v-payment-handle.tsx
@@ -1,4 +1,4 @@
-import { Component, Event, EventEmitter, Host, Listen, Prop, h } from '@stencil/core';
+import { Component, Event, EventEmitter, State, Host, Listen, Prop, h, FunctionalComponent } from '@stencil/core';
 import { MatchResults, RouterHistory, injectHistory } from '@stencil/router';
 
 @Component({
@@ -26,12 +26,16 @@ export class VPaymentHandle {
   @Prop() match: MatchResults;
   @Prop() history: RouterHistory;
 
+  @State() isFetched_ViewData: boolean = false;
+
   private id_Session: string;
 
   componentWillLoad() {
     if (this.match.params.id_Session) {
       this.id_Session = this.match.params.id_Session.trim();
     }
+
+    console.log(this.id_Session);
   }
 
   componentDidLoad() {
@@ -44,25 +48,33 @@ export class VPaymentHandle {
     console.log(`fetching session details: ${this.id_Session}`);
   }
 
+  UI_Skel: FunctionalComponent = () => (
+    <c-card>
+      <div class="skel__line"></div>
+      <div class="skel__line"></div>
+      <div class="skel__line"></div>
+    </c-card>
+  );
+
+  UI_Default: FunctionalComponent = () => (
+    <c-card>
+      <e-text variant="display" theme="success">
+        Payment Successful
+      </e-text>
+      <l-spacer value={1}></l-spacer>
+      <e-text>
+        Thank you for purchasing <strong>Aitihya, the Heritage - Vol XVIII, Issue 2</strong>
+      </e-text>
+      <e-text>It has been added to your library.</e-text>
+      <l-spacer value={1}></l-spacer>
+      <e-link action="myLibrary" event={true}>
+        <strong>Go to library</strong>
+      </e-link>
+    </c-card>
+  );
+
   render() {
-    return (
-      <Host>
-        <c-card>
-          <e-text variant="display" theme="success">
-            Payment Successful
-          </e-text>
-          <l-spacer value={1}></l-spacer>
-          <e-text>
-            Thank you for purchasing <strong>Aitihya, the Heritage - Vol XVIII, Issue 2</strong>
-          </e-text>
-          <e-text>It has been added to your library.</e-text>
-          <l-spacer value={1}></l-spacer>
-          <e-link action="myLibrary" event={true}>
-            <strong>Go to library</strong>
-          </e-link>
-        </c-card>
-      </Host>
-    );
+    return <Host>{this.isFetched_ViewData ? <this.UI_Default></this.UI_Default> : <this.UI_Skel></this.UI_Skel>}</Host>;
   }
 }
 
