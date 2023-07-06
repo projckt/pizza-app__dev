e39b44dcf5e3b1708eb6bce3b71da1321dc8f2d8
Projckt <projckt@protonmail.com>
2023-07-06 10:41:00 +0530

    removes build errors

--- app-frontend/src/components/app-root/app-root.tsx
+++ app-frontend/src/components/app-root/app-root.tsx
@@ -1,6 +1,6 @@
 import { Component, Prop, Listen, h } from '@stencil/core';
 import { RouterHistory, injectHistory } from '@stencil/router';
-import { state, IO, init_Socket } from '../../global/script';
+import { state, IO } from '../../global/script';
 import { helper_Set_State, helper_Set_Session_In_LocalStorage, helper_Check_Session_In_LocalStorage } from './helpers';
 import { Helper_ApiCall_GetAccountDetails_BySession } from '../../global/script/helpers';
 
